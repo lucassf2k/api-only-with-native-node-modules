3f56b2aebdce3673a1c1fc960d7729fe74a851d8
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-01-28 15:33:35 -0300

    feat(controllers/HeroesController): getheroById method created

--- src/controllers/HeroesController.mjs
+++ src/controllers/HeroesController.mjs
@@ -2,7 +2,28 @@ import { heroes } from '../mocks/heroes.mjs';
 
 export class HeroesController {
   list(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(heroes));
+    const { order } = req.query;
+
+    const sortedHeroes = heroes.sort((a, b) => {
+      if (order === 'desc') {
+        return a.id < b.id ? 1 : - 1;
+      }
+
+      return a.id > b.id ? 1 : - 1;
+    });
+
+    res.send(200, sortedHeroes);
+  }
+
+  getHeroById(req, res) {
+    const { id } = req.params;
+
+    const hero = heroes.find((hero) => hero.id === Number(id));
+
+    if (!hero) {
+      return res.send(400, { error: 'Hero not found' });
+    } 
+
+    res.send(200, hero);
   }
 }
