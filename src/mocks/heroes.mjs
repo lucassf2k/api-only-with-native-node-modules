54843b934d2efd97cd48d1a19cf2ca747694c041
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-01-28 14:04:02 -0300

    feat: create mocks of heroes

---
+++ src/mocks/heroes.mjs
@@ -0,0 +1,27 @@
+export const heroes = [
+  {
+    id: 0,
+    name: 'Superman',
+    publisher: 'DC Comics'
+  },
+  {
+    id: 1,
+    name: 'Spiderman',
+    publisher: 'Marvel'
+  },
+  {
+    id: 2,
+    name: 'Supergirl',
+    publisher: 'DC'
+  },
+  {
+    id: 3,
+    name: 'Wonder Woman',
+    publisher: 'DC'
+  },
+  {
+    id: 4,
+    name: 'Jean Grey',
+    publisher: 'Marvel'
+  },
+];
