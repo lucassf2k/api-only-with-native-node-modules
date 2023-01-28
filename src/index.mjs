48902cb23bce63092c8807a57c0c718fdc9512ee
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-01-28 15:36:37 -0300

    refactor(index): better handling the url and query, params created in the request and send created in the response

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,39 @@
 import http from 'node:http';
+import { URL } from 'node:url';
+
+import { routes } from './routes.mjs';
 
 const server = http.createServer((req, res) => {
+  const parseUrl = new URL(`http://localhost:3333${req.url}`);
+  console.log(`Request method: ${req.method} | Endpoint: ${parseUrl.pathname}`);
+
+  let { pathname } = parseUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+ 
+  const route = routes.find((routeObj) => 
+    routeObj.endpoint === pathname && routeObj.method === req.method
+  );
+
+  if (route) {
+    req.query = Object.fromEntries(parseUrl.searchParams);
+    req.params = { id };
+
+    res.send = (statusCode, body) => {
+      res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(body));
+    };
+
+    route.handler(req, res);
+  } else {
+    res.send(404, `Cannot ${req.method} ${parseUrl.pathname}`);
+  }
 
 });
 
