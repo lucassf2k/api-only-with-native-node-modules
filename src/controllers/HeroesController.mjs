5765303193b228431fb0448793acdcf1e1691f8b
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-01-28 21:50:53 -0300

    feat(controllers/HeroesController): add methods updateHero and deleteHero

--- src/controllers/HeroesController.mjs
+++ src/controllers/HeroesController.mjs
@@ -1,4 +1,6 @@
-import { heroes } from '../mocks/heroes.mjs';
+import { heroes as HEROES } from '../mocks/heroes.mjs';
+
+let heroes = HEROES;
 
 export class HeroesController {
   list(req, res) {
@@ -25,5 +27,57 @@ export class HeroesController {
     } 
 
     res.send(200, hero);
+  }
+
+  createHero(req, res) {
+    const { body } = req;
+
+    const lastHero = heroes[heroes.length - 1].id;
+    const newHero = { 
+      id: lastHero + 1,
+      name: body.name,
+      publisher: body.publisher, 
+    };
+
+    heroes.push(newHero);
+
+    res.send(201, newHero);
+  }
+
+  updateHero(req, res) {
+    let { id } = req.params;
+    const { name, publisher } =  req.body;
+    
+    id = Number(id);
+
+    const heroExists = heroes.find((hero) => hero.id === id);
+
+    if (!heroExists) {
+      return res.send(400, { error: 'Hero not found' });
+    }
+
+    heroes.forEach((hero) => {
+      if (hero.id === id) {
+        hero.name = name;
+        hero.publisher = publisher;
+      }
+    });
+
+    res.send(200, { id, name, publisher });
+  }
+
+  deleteHero(req, res) {
+    let { id } = req.params;
+    id = Number(id);
+
+    const heroFiltered = heroes.find((hero) => hero.id === id);
+
+    if (!heroFiltered) {
+      return res.send(400, { error: 'Hero not found!' });
+    }
+
+    heroes = heroes.filter((hero) => !(hero.id === heroFiltered.id));
+
+    res.send(200, { deleted: true });
   }
 }
